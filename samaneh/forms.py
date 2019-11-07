b7d5da96149a35c04c56c1f7516a9c74f5333598
mgh_2000 <m.aghasemzadeh@yahoo.com>
2019-11-07 16:38:37 +0330

    nothing

--- samaneh/forms.py
+++ samaneh/forms.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
 
+from . import models
 
-class SignUpForm(UserCreationForm):
+
+class SignUpForm(forms.ModelForm):
+    password2 = forms.CharField(max_length=100)
+
     class Meta:
-        first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-        last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-        email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-        model = User
-        fields = ('first_name', 'last_name','username', 'email', 'password1', 'password2', )
+        model = models.User
+        fields = '__all__'
